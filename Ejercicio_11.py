33442240de9cc254200a43e3b7ed20ad6f86b14e
lucianomasuelli <lucianomasuelli.lm@gmail.com>
2021-06-20 23:21:33 -0300

    modifico ejercicio 11

--- Ejercicio_11.py
+++ Ejercicio_11.py
@@ -1,30 +1,6 @@
 from algo1 import *
 from linkedlist import *
 
-def transition_function(pattern,sigma):
-    m = len(pattern)+1
-    n = length(sigma)
-    matrix = Array(m,Array(n,0))
-    for q in range(0,m): #recorre todos los estados.
-        Pq = str(substr(pattern,0,q))
-        current = sigma.head
-        for a in range(0,n): #recorre todos los elementos del alfabeto.
-            PqA = String(Pq + current.value)
-            if q != m-1:  
-                k = substr(pattern,0,q+1)
-            else: #si se está analizando el ultimo estado, a PqA le quito el primer elemento de la cadena para que tenga el mismo tamaño de k.
-                k = pattern
-                PqA = substr(PqA,1,len(PqA))
-            while strcmp(k,PqA) == False and len(k) != 0: #busca el mayor prefijo de "k" que sea sufijo de "PqA".
-                k = substr(k,0,len(k)-1)
-                PqA = substr(PqA,1,len(PqA))
-            matrix[q][a] = len(k) #guardo el largo del mayor prefijo en una matriz.
-            current = current.nextNode
-
-    return matrix
-
-
-
 def find_pattern(T,P):
     if len(P) > len(T):
         #si P es de mayor tamaño que T, lo acorto quitandole elementos del final.
